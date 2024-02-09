6e410ee6b5d017bc4d6836875b91122da7c05205
John D <johnd@Johns-MBP.attlocal.net>
2024-02-09 11:44:20 -0600

    model work

---
+++ models/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+
+const user = require('./user');
+const jobs = require('./jobs');
+const { Module } = require('module');
+const applications = ('./applications.js')
+
+
+module.exports = router;
---
+++ models/jobs.js
@@ -0,0 +1,47 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+
+class jobs extends Model {}
+
+jobs.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+    },
+    
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'jobs',
+  }
+);
+
+
+
+
+
+
+
+
+module.exports = jobs;
