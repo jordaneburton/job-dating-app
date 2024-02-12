058688063e3b158ec66e98955bf0515d6bc82086
Jordan <jordane.burton@gmail.com>
2024-02-12 13:06:22 -0600

    setup for rendering to views, nonfunctional

---
+++ controllers/pageControllers.js
@@ -0,0 +1,57 @@
+const User = require('../models/user');
+
+module.exports = {
+    getFunc: async (req, res) => {
+        try {
+            res.status(200).json({message: 'SUCCESS: GET connected to pageRoutes'})
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    getHome: async (req, res) => {
+        try {
+            return res.render('homepage', {
+                // logged_in: req.session.logged_in 
+            });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    getJobs: async (req, res) => {
+        try {
+            return res.render('homepage', {
+                // logged_in: req.session.logged_in 
+            });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    getProfile: async (req, res) => {
+        try {
+            // Get all projects and JOIN with user data
+            const userData = await User.findByPk(req.params.id);
+        
+            // Serialize data so the template can read it
+            const user = userData.get({ plain: true });
+        
+            return res.render('homepage', {
+                // attributes: { exclude: ['password'] }
+                user     // ONLY ONE OF THESE SHOULD BE USED
+            });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+
+    getLogin: async (req, res) => {
+        try {
+            return res.render('homepage', {
+            });
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    },
+}
---
+++ routes/pageRoutes.js
@@ -0,0 +1,26 @@
+const router = require('express').Router();
+const { getFunc, 
+        getHome, 
+        getJobs, 
+        getProfile, 
+        getLogin 
+    } = require('../controllers/pageControllers');
+const { withAuth } = require('../utils/helpers');
+
+
+router.route('/') // POST-PRODUCTION home page router
+.get(getFunc);
+
+router.route('/home') // TEMPORARY home page router
+.get(getHome)
+
+router.route('/jobs')
+.get(getJobs)
+
+router.route('/profile')
+.get(getProfile)
+
+router.route('/login')
+.get(getLogin)
+
+module.exports = router;
