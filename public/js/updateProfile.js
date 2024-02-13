60629db333a365b589fbc3bbeca547c6c977130d
Michael Davidson <msdavid55@gmail.com>
2024-02-13 12:42:31 -0600

    moved around function variables to make it more legible

--- public/js/updateProfile.js
+++ public/js/updateProfile.js
@@ -1,14 +1,15 @@
-const firstName = document.querySelector('#profile-first').value.trim();
-const lastName = document.querySelector('#profile-last').value.trim();
-const recentJob = document.querySelector('#profile-recent-job').value.trim();
-const category = document.querySelector('#profile-category').value.trim();
-const salary = document.querySelector('#profile-salary').value.trim();
-const bio = document.querySelector('#profile-bio').value.trim();
 
 const profileFormHandler = async (event) => {
-
-    // Stop the browser from submitting the form so we can do so with JavaScript
-    event.preventDefault();
+  
+  // Stop the browser from submitting the form so we can do so with JavaScript
+  event.preventDefault();
+  
+  const firstName = document.querySelector('#profile-first').value.trim();
+  const lastName = document.querySelector('#profile-last').value.trim();
+  const recentJob = document.querySelector('#profile-recent-job').value.trim();
+  const category = document.querySelector('#profile-category').value.trim();
+  const salary = document.querySelector('#profile-salary').value.trim();
+  const bio = document.querySelector('#profile-bio').value.trim();
   
     // Gather the data from the form elements on the page
     if (firstName && lastName && recentJob && category && salary && bio) {
