6497a83864e2265c5e4312b09fa35807a5e6742c
Jordan <jordane.burton@gmail.com>
2024-02-13 23:54:25 -0600

    updated seed file

--- seeds/seed.js
+++ seeds/seed.js
@@ -6,18 +6,6 @@ const seedJob = require('./jobSeed.json');
 const seedUser = require('./userSeed.json');
 
 const seedAll = async () => {
-<<<<<<< HEAD
-    await sequelize.sync({ force: true });
-    
-    await User.bulkCreate(seedUser, {
-        individualHooks: true,
-        returning: true,
-    });
-
-    await Jobs.bulkCreate(seedJob)
-
-    process.exit(0);
-=======
   await sequelize.sync({ force: true });
 
   await User.bulkCreate(seedUser, {
@@ -28,7 +16,6 @@ const seedAll = async () => {
     await Jobs.bulkCreate(seedJob)
 
   process.exit(0);
->>>>>>> 57b818a792852e3f4b0ccd315d98012081dcb644
 };
 
 seedAll();
